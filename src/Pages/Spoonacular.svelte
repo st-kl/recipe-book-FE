fb6a3e76719c79ea75b80508299d566a7f8128e8
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-07-29 11:35:40 +0100

    add SideNav with child components to Spoonacular page

--- src/Pages/Spoonacular.svelte
+++ src/Pages/Spoonacular.svelte
@@ -1,3 +1,17 @@
+<script>
+    import SideNav from '../Components/SideNav.svelte';
+    import Profile from '../Components/Profile.svelte';
+    import Search from '../Components/Search.svelte';
+    import Filter from '../Components/Filter.svelte';
+    import RecipeScrapeUrl from '../Components/RecipeScrapeUrl.svelte';
+</script>
+
 <div>
     <h1>Spoonacular Page</h1>
+    <SideNav>
+        <Profile />
+        <Search />
+        <Filter />
+        <RecipeScrapeUrl />
+    </SideNav>
 </div>
