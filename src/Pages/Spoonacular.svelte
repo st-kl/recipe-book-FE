f1d6764929a004f9258ddedb1247230a0af408cd
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-07-29 11:57:14 +0100

    add cuisines and search bar to Spoonacular

--- src/Pages/Spoonacular.svelte
+++ src/Pages/Spoonacular.svelte
@@ -4,7 +4,11 @@
     import Search from '../Components/Search.svelte';
     import Filter from '../Components/Filter.svelte';
     import RecipeScrapeUrl from '../Components/RecipeScrapeUrl.svelte';
+
+    const cuisines = ['african', 'chinese', 'japanese', 'korean', 'vietnamese', 'thai', 'indian', 'british', 'irish', 'french', 'italian', 'mexican', 'spanish', 'middle eastern', 'jewish', 'american', 'cajun', 'southern', 'greek', 'german', 'nordic', 'eastern european', 'caribbean','latin american'];
+
 </script>
+
 
 <div>
     <h1>Spoonacular Page</h1>
@@ -14,4 +18,15 @@
         <Filter />
         <RecipeScrapeUrl />
     </SideNav>
+    <h1>Categories:</h1>
+    <ul>
+        <h3>Cuisines:</h3>
+        {#each cuisines as cuisine}
+        <li>{cuisine}</li>
+        {/each}
+    </ul>
+    <form>
+        <input id="search" placeholder="Enter search term here">
+        <button type="submit">Submit</button>
+    </form>
 </div>
