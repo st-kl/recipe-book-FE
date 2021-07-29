bc25846c237bb5cef7ddad37257b76398b6d0846
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-07-29 11:18:18 +0100

    add spoonacular link to Nav

--- src/Components/Nav.svelte
+++ src/Components/Nav.svelte
@@ -1,9 +1,11 @@
 <script>
     import { Router, Link, Route } from 'svelte-navigator'
+import Spoonacular from '../Pages/Spoonacular.svelte';
     </script>
     
     <nav>
         <Link to="/">Login</Link>
         <Link to="home">Home</Link>
        <Link to="mycookbook">MyCookBook</Link>
+       <Link to='spoonacular'>Spoonacular</Link>
     </nav>
