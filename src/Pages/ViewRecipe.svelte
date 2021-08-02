2f99b0140ad8fb8f292b32eb55f9ee83096b3880
Kev <kpmorel@gmail.com>
2021-08-02 12:21:36 +0100

    can fetch individual recipe based on url recipe id perams

--- src/Pages/ViewRecipe.svelte
+++ src/Pages/ViewRecipe.svelte
@@ -1,23 +1,38 @@
 <script>
-import axios from 'axios';
-import config from '../../config';
-import {onMount} from 'svelte'
-import {useParams} from 'svelte-navigator'
-    const params = useParams()
-    let recipeId = params
+  import axios from 'axios';
+  import config from '../../config';
+  import { onMount } from 'svelte';
+  import { useParams } from 'svelte-navigator';
+  const params = useParams();
 
-    let options = { headers: {
-        'x-rapidapi-key': config.key,
-        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'}}
+  // id = $params.id
 
-     onMount (async() => {
-         let response = await axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeId}/summary`, options)}).
-         then((response) => { console.log(response.data)})
+  //   let options = {
+  //     headers: {
+  //       'x-rapidapi-key': config.key,
+  //       'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+  //     },
+  //   };
+  console.log($params.id, 'PARAMS<<<<<<<'); 
 
-    
-
+  onMount(async () => {
+    let response = await axios
+      .get(
+        `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${$params.id}/summary`,
+        {
+          headers: {
+            'x-rapidapi-key': config.key,
+            'x-rapidapi-host':
+              'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
+  });
 </script>
-<div>
-    <h1>View Recipe Page</h1>
 
+<div>
+  <h1>View Recipe Page</h1>
 </div>
