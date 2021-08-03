35cdb24bf2536f36b410d63a5eae44f5a31f0fdd
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-08-03 14:48:40 +0100

    add display recipe steps functionality

--- src/Pages/PlayRecipe.svelte
+++ src/Pages/PlayRecipe.svelte
@@ -3,30 +3,48 @@
     import {onMount} from 'svelte';
     import axios from 'axios';
     import SideNav from '../Components/SideNav.svelte';
+    import Card from '../Components/Card.svelte';
     const params = useParams();
     let { id } = $params;
     let recipe;
     let ingredients = [];
+    let steps = [];
+    let instructions = [];
 
     onMount(async () => {
         let response = await axios.get(`https://mycookeroo.herokuapp.com/api/recipes?recipeId=${id}`).then((response) => {
            recipe = response.data.recipes[0];
            ingredients = recipe.ingredients;
-           console.log(recipe);
-           console.log(ingredients);
+           !recipe.steps ? instructions = recipe.instructions : steps = recipe.steps;
         });
     });
 </script>
 
 <div>
     <SideNav>
-        <h1>Play Recipe Page</h1>
         <h3>Ingredients:</h3>
         {#each ingredients as {name, amount, unit}}
         <label>
             <input type=checkbox>
-            {amount} {unit} {name}
+            {amount} {unit} {name} 
         </label>
         {/each}
     </SideNav>
+    <main class="steps">
+        {#if steps.length !== 0}
+        {#each steps as {number, step, instructions}}
+        <Card>
+            <p>{number}: {step}</p>
+        </Card>
+        {/each}
+        {:else}
+        {#each instructions as instruction, i}
+        <Card>
+            <p>{i + 1}: {instruction}</p>
+        </Card>
+        {/each}
+        {/if}
+    </main>
 </div>
+
+
