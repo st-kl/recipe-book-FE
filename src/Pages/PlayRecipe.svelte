955172f5178614769b40ce9897823fea7571d7ac
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-08-03 12:07:07 +0100

    add useParams method to get recipeId

--- src/Pages/PlayRecipe.svelte
+++ src/Pages/PlayRecipe.svelte
@@ -1,3 +1,11 @@
+<script>
+    import {useParams} from 'svelte-navigator';
+    const params = useParams();
+    let recipeId = $params;
+    console.log(recipeId);
+</script>
+
 <div>
     <h1>Play Recipe Page</h1>
+    <p>{recipeId.id}</p>
 </div>
