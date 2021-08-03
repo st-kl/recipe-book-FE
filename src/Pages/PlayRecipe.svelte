e986cb04bb0499e3ca512f5aa5283bdec4c8e93b
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-08-03 12:44:50 +0100

    aadd get request for recipe by id

--- src/Pages/PlayRecipe.svelte
+++ src/Pages/PlayRecipe.svelte
@@ -1,11 +1,22 @@
 <script>
     import {useParams} from 'svelte-navigator';
+    import {onMount} from 'svelte';
+    import axios from 'axios';
     const params = useParams();
-    let recipeId = $params;
-    console.log(recipeId);
+    let { id } = $params;
+    let recipe;
+    let ing = [];
+
+    onMount(async () => {
+        let response = await axios.get(`https://mycookeroo.herokuapp.com/api/recipes?recipeId=${id}`).then((response) => {
+           recipe = response.data.recipes[0];
+           ing = recipe.ingredients;
+           
+        });
+    });
 </script>
 
 <div>
     <h1>Play Recipe Page</h1>
-    <p>{recipeId.id}</p>
+    <p>{id}</p>
 </div>
